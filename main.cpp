9e590461020669fa9cbac160e4c2fbe3b6c54efe
Saruchisah <saruchi344@gmail.com>
2026-06-09 11:49:38 +0530

    Added menu-driven interface

--- main.cpp
+++ main.cpp
@@ -3,27 +3,71 @@ using namespace std;
 
 int main()
 {
-    string expense[3];
-    float amount[3];
+    string expense[100];
+    float amount[100];
+    int count = 0;
+    int choice;
     float total = 0;
 
-    for(int i=0;i<3;i++)
+    do
     {
-        cout<<"Enter Expense Name: ";
-        cin>>expense[i];
+        cout << "\n===== EXPENSE TRACKER =====\n";
+        cout << "1. Add Expense\n";
+        cout << "2. View Expenses\n";
+        cout << "3. Show Total Expense\n";
+        cout << "4. Exit\n";
+        cout << "Enter Choice: ";
+        cin >> choice;
 
-        cout<<"Enter Amount: ";
-        cin>>amount[i];
+        switch(choice)
+        {
+            case 1:
+                cout << "Enter Expense Name: ";
+                cin >> expense[count];
 
-        total += amount[i];
-    }
-    cout<<"\nExpenses:\n";
+                cout << "Enter Amount: ";
+                cin >> amount[count];
 
-    for(int i=0;i<3;i++)
-    {
-        cout<<expense[i]<<" : Rs. "<<amount[i]<<endl;
-    }
-    cout<<"\nTotal Expense = Rs. "<<total;
+                total += amount[count];
+                count++;
+
+                cout << "Expense Added Successfully!\n";
+                break;
+
+            case 2:
+                cout << "\n----- Expense List -----\n";
+
+                if(count == 0)
+                {
+                    cout << "No Expenses Added Yet!\n";
+                }
+                else
+                {
+                    for(int i = 0; i < count; i++)
+                    {
+                        cout << i + 1 << ". "
+                             << expense[i]
+                             << " - Rs. "
+                             << amount[i]
+                             << endl;
+                    }
+                }
+                break;
+
+            case 3:
+                cout << "\nTotal Expense = Rs. "
+                     << total << endl;
+                break;
+
+            case 4:
+                cout << "\nThank You!\n";
+                break;
+
+            default:
+                cout << "\nInvalid Choice!\n";
+        }
+
+    } while(choice != 4);
 
     return 0;
 }
